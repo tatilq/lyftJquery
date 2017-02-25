1edf38ee0fc4fbaa69b154dbc97c6ed4743b32b4
Tatiana Limache Quispe <tati34che@gmail.com>
2017-02-24 19:11:58 -0500

    Añadi sweetAlert

--- js/sign-up.js
+++ js/sign-up.js
@@ -23,26 +23,17 @@ function regresar()
 /****************************Funcion que valida que el  Codigo exista en mi LocalStorage***************************/
 function  validaLocalStorageCode()
 {
-	var exist=false;
-	if(localStorage.getItem('codigo') != null)
-		exist=true;
-	return exist;
+	return (localStorage.getItem('codigo') != null);
 }
 /****************************Funcion que el 'SRC' de la imagen exista en mi LocalStorage***************************/
 function  validaLocalStorageImage()
 {
-	var exist=false;
-	if(localStorage.getItem('srcImgPais') != null)
-		exist=true;
-	return exist;
+	return(localStorage.getItem('srcImgPais') != null);
 }
 /*****************************FUNCION QUE VALIDA EXPRESION REGULAR DE SOLO NUMEROS------***********************/
 function isNumberPhone(number)
 {
-	var isValid=false;
-	if (/^([0-9])*$/.test(number))
-	    isValid= true;
-	return isValid;
+	return(/^([0-9])*$/.test(number));
 }
 /*****************************FUNCION QUE VALIDA LA LONGITUD MAX 9 CARACTERES DEL TELEFONO***********************/
 function validaLongitud()
@@ -54,34 +45,37 @@ function validaLongitud()
 	if(isNumberPhone(numero))
 	{	
 		if(numero.length<9 || numero.length>9  )
-		{
-			salida.html("<span style='color:red; font-style:italic;'>Max 9 numeros</span>"); 
-		}
+			salida.html("<span style='color:red; font-style:italic;'>Max 9 numeros</span>");
 		else
 		{
 			salida.html("<span style='color:green; font-style:italic;'>Numero Valido</span>"); 
 			isValid=true;
 		}
 	}
-	else{
+	else
 		salida.html("<span style='color:red; font-style:italic;'>Incorrecto, solo numeros</span>");
-	}
+	
 	return isValid;
 }
-/****************************************FUNCION RAMDOOM PARA NUMEROS DE 3 CIFRAS*******************************/
-function getRandom(a,b) 
-{
-    return Math.random() *((a-b) + b);
-}
+
 /***********************FUNCION QUE SOLO GENERA EL CODIGO ALEATORIO SI EL TELEFONO ES CORRECTO*****************/
 function  getCodigo()
 {
 	if(validaLongitud())
 	{
-		var codigo= "LAB-"+Math.floor(getRandom(999,100));
+		var codigo= "LAB-"+Math.floor(Math.random()*(999)+100);
 		localStorage.setItem('codigoLab',codigo);
-	    alert("su codigo es: "+codigo);
-	    window.location="sign-up2.html";
+	    swal({
+	      title: "Se generó tu código",
+	      text: "Tú código de usuario es:" + codigo,
+	      type: "info"
+	    },function(isConfirm){
+	      	if (isConfirm)
+	      	{
+	        	swal("Código aceptado", "", "success");
+	        	setTimeout(function(){window.location="sign-up2.html";}, 50);
+	        } 
+	    });
 	}
 	else
 	{
@@ -89,4 +83,3 @@ function  getCodigo()
 		$('#numero').val('');
 	}	
 }
-/***************************************************FIN*********************************************************/
