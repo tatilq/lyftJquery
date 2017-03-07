4bd11f38bbd2e09daa0833ab8a2952b0266a2b6a
Tatiana Limache Quispe <Tati34che@gmail.com>
2017-03-07 17:44:01 -0500

    Cambios en localstorage

--- js/app.js
+++ js/app.js
@@ -2,85 +2,22 @@ $(document).ready(init);
 //--------------------------------------FUNCION QUE OBTIENE TU POSICION ACTUAL-----------------------------------//
 function init()
 {
-  $('.dropdown-menu').css({"posistion":"absolute" , "bottom":"100%" , "top":"initial"});
-  navigator.geolocation.watchPosition(mostrar,gestionarErrores);
-  solicitarEstimado();
-
+    $('.dropdown-menu').css({"posistion":"absolute" , "bottom":"100%" , "top":"initial"});
+ 	asignaEventoClick();
 }
-
-function solicitarEstimado()
+function asignaEventoClick()
 {
-  $.ajax({
-        url:'https://clientes.geekadvice.pe/api/estimado',
-        data: {tipo:'2'}
-        }).done(function(_data){
-            console.log(_data);
-            update(_data);
-        });
-
+	var lis=$('.li');
+	$.each(lis, function() {$(this).on('click',onLinkClick)});
 }
-function update(_info)
+/****************************FUNCION QUE GUARDA LOCALMENTE LA 'SRC' DE LA IMAGEN Y EL 'CODIGO' DEL PAIS*******************/
+function onLinkClick(evt)
 {
-    //alert(_info.destino);
-    //alert(_info.estimado.moneda);
-    //alert(_info.estimado.min);
-    //alert(_info.estimado.max);
-    $('#priceEstimate').text(_info.estimado.moneda+_info.estimado.min+'-'+_info.estimado.max);
+	var srcCar= $(evt.currentTarget).find('.logo').attr('src');//  $('.bandera').eq(0).attr('src');
+	var type= $(evt.currentTarget).find('.type').text();
+	var name= $(evt.currentTarget).find('.name').text();
+	localStorage.setItem('srcImgCar',srcCar);
+	localStorage.setItem('nameCar',name);
+	localStorage.setItem('typeCar',type);
+	
 }
-
-//------------------------------ --------FUNCION QUE OBTINEN MI POSICION ACTUAL-----------------------------------//
-function mostrar(posicion)
-{
-  var lati=posicion.coords.latitude;
-  var longi=posicion.coords.longitude;
-  initMap(lati,longi);
-  //map.setCenter("");
-  /*$('#lista').on("click",".justificar",function()
-  {
-    console.log($(this));
-  });*/
-}
-//--------------------------------------FUNCION PARA GESTIONAR ALGUN TIPO DE ERROR------------------------------//
-function gestionarErrores(error)
-{
-  alert('Error: '+error.code+' '+error.message+ '\n\nPor favor compruebe que está conectado '+
-  'a internet y habilite la opción permitir compartir ubicación física');
-}
-//-----------------FUNCION QU CARGA EL MAPA, CON LA LATITUD Y LONGITUD DE TU POSICION ACTUAL--------------------//
-function initMap(lati, longi) 
-{
-  var positionActual={lat: lati, lng: longi};
-  var position1={lat: lati-0.004 , lng: longi+0.01};
-  var position2={lat: lati+0.009 , lng: longi+0.001};
-  var position3={lat: lati+0.003, lng: longi-0.01};
-  var map = new google.maps.Map(document.getElementById('map'),{ center: positionActual, zoom: 14});
-
-  var marker = new google.maps.Marker({//parametro recibe un objeto
-    position: positionActual,
-    map: map ,     
-    title: 'Aqui Estoy¡',
-    icon:'img/persona.png'
-  });
-
-  var marker1 = new google.maps.Marker({//parametro recibe un objeto
-    position: position1,
-    map: map ,     
-    title: 'BatiMovil1',
-    icon:'img/car.png'
-  });
-    
-  var marker2 = new google.maps.Marker({//parametro recibe un objeto
-    position: position2,
-    map: map ,     
-    title: 'BatiMovil2',
-    icon:'img/car.png'
-  });
-
-  var marker3 = new google.maps.Marker({//parametro recibe un objeto
-    position: position3,
-    map: map ,     
-    title: 'BatiMovil3',
-    icon:'img/car.png'
-  });
-} 
-//---------------------------------------------------FIN-------------------------------------------------//
--- js/app_1.js
+++ js/app_1.js
@@ -2,19 +2,25 @@ $(document).ready(init);
 //-------------------------------------------------------------------------//
 function init()
 {
+
   solicitarEstimado();
 }
 
 function solicitarEstimado()
 {
+	var type=localStorage.getItem('typeCar');
   $.ajax({
     url:'https://clientes.geekadvice.pe/api/estimado',
-    data: {tipo:'2'}
+    data: {tipo:type}
     }).done(function(_data){
         update(_data);
     });
 }
 function update(_info)
 {
+	$('#car').attr({'src': localStorage.getItem('srcImgCar')});
+	$('#nameCar').text(localStorage.getItem('nameCar'));
+	
     $('#priceEstimate').text(_info.estimado.moneda+_info.estimado.min+'-'+_info.estimado.max);
 }
+
