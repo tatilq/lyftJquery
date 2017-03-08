7982fef0456819e2c4c16ae4cdaf4c8d03ca4afb
Tatiana Limache Quispe <Tati34che@gmail.com>
2017-03-08 04:29:41 -0500

    Añadi sweetAlert para seleecionar vehiculos

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
+var select=false;
 $(document).ready(init);
 //--------------------------------------FUNCION QUE OBTIENE TU POSICION ACTUAL-----------------------------------//
 function init()
-{
+{	
     $('.dropdown-menu').css({"posistion":"absolute" , "bottom":"100%" , "top":"initial"});
+    $('#btn').click(setPickup);
  	asignaEventoClick();
 }
 function asignaEventoClick()
@@ -13,6 +15,7 @@ function asignaEventoClick()
 /****************************FUNCION QUE GUARDA LOCALMENTE LA 'SRC' DE LA IMAGEN Y EL 'CODIGO' DEL PAIS*******************/
 function onLinkClick(evt)
 {
+	select=true;
 	var srcCar= $(evt.currentTarget).find('.logo').attr('src');
 	var type= $(evt.currentTarget).find('.type').text();
 	var name= $(evt.currentTarget).find('.name').text();
@@ -21,5 +24,31 @@ function onLinkClick(evt)
 	localStorage.setItem('nameCar',name);
 	localStorage.setItem('typeCar',type);
 	localStorage.setItem('seat',seats);
+
+}
+function setPickup()
+{
+	if(select)
+	{
+		swal({
+		    title: "¡Se selecciono exitosamente!",
+		    text: "Tú vehiculo de destino es:" + localStorage.getItem('nameCar').toUpperCase(),
+		    imageUrl: "img/likes.png"
+		},function(isConfirm){
+	      	if (isConfirm)
+	      	{
+	        	setTimeout(function(){window.location="app_1.html";}, 50);
+	        } 
+		});
+	}
+	else
+	{
+		swal({
+		  title: "¡Selecciona el Vehiculo de destino!",
+		  text: "No deleccionaste ningun vehiculo",
+		  imageUrl: "img/deslikes.png"
+		});
+	}
+
 }
 /*****************************************FIN***************************************************/
