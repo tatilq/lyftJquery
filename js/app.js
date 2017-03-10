b9507241faf6f3a3882eeca5545889060f8d1f3f
Tatiana Limache Quispe <Tati34che@gmail.com>
2017-03-10 07:51:58 -0500

    Solucion de error https

--- js/app.js
+++ js/app.js
@@ -28,6 +28,7 @@ function init()
     carList.append(html);
   	
   	});
+  	/****LLAMA A LA FUNCION QUE ASIGNA EL EVENTO CLICK***/
    	asignaEventoClick();
 }
 function asignaEventoClick()
--- js/carJSON.js
+++ js/carJSON.js
@@ -1,5 +1,6 @@
-var cars = '[{"nombre": "Line","time":"3", "type":"1", "image":"http://www.toyota.com.ec/gt86/web/css/images/historycar2.png", "description":"Shared, 2 riders max"},'+
+
+var cars = '[{"nombre": "Line","time":"3", "type":"1", "image":"img/carLine.png", "description":"Shared, 2 riders max"},'+
 	'{"nombre": "Lyft","time":"3", "type":"2", "image":"img/carLyft.png" , "description":"Fast ride, 4 seats"},'+
-	'{"nombre": "Plus","time":"4", "type":"3", "image":"http://24-7carrental.com/img/car1.png", "description":"Fast ride, 6 seats"},'+
+	'{"nombre": "Plus","time":"4", "type":"3", "image":"img/carPlus.png", "description":"Fast ride, 6 seats"},'+
 	'{"nombre": "Premier","time":"3", "type":"4", "image":"img/carPremier.png", "description":"High-end, 4 seats"}]';
    
